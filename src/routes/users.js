5f5f1edefdc9363cac1b312ff9ee3fbc70382193
Boris Belmar <borisbelmarm@gmail.com>
2019-06-24 23:44:41 -0400

    Avanzando en la app, arreglando vistas

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,8 +4,13 @@ const router = express.Router();
 router.get('/login', (req, res) => {
     res.render('./users/login');
 });
+
 router.get('/signup', (req, res) => {
     res.render('./users/signup');
+});
+
+router.get('/profile', (req, res) => {
+    res.render('./users/profile');
 });
 
 module.exports = router;
